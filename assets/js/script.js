d24a8645766c96ae42b4f0ee4436f082c73d8a1f
ianmeigh <ianmeigh@googlemail.com>
2021-12-13 16:57:59 +0000

    FEAT: Add code to generate random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   let buttons = document.getElementsByTagName('button');
   for (let button of buttons) {
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
       if (this.getAttribute("data-type") === "submit") {
         console.log("You clicked submit!");
       } else {
@@ -15,7 +15,20 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
-function runGame() { }
+/**
+ * The main game "loop", called when the script is first loaded and after the
+ * user's answer has been processed.
+ */
+function runGame() {
+
+  function generateRandomNumber(min, max) {
+    return (Math.floor(Math.random() * max) + min);
+  }
+
+  let num1 = generateRandomNumber(1, 25);
+  let num2 = generateRandomNumber(1, 25);
+}
+
 function checkAnswer() { }
 function calculateCorrectAnswer() { }
 function incrementScore() { }
