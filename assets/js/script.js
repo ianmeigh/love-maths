1683e6972143221bd7973bc978e211982d2f4a7a
ianmeigh <ianmeigh@googlemail.com>
2021-12-13 16:13:29 +0000

    FEAT: Add structure to game functions

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function runGame() {}
+function checkAnswer() {}
+function calculateCorrectAnswer()  {}
+function incrementScore() {}
+function displayQuestion() {}
+function displayAdditionQuestion() {}
+function displaySubtractQuestion() {}
+function displayMultiplyQuestion() {}
