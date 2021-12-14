b1ad74ab1adb861f862664a53677aab1eddbf2c1
ianmeigh <ianmeigh@googlemail.com>
2021-12-14 11:24:30 +0000

    FEAT: Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,17 +9,19 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log("You clicked submit!");
       } else {
         let gameType = this.getAttribute("data-type");
-        console.log(`You clicked ${gameType}!`);
+        runGame(gameType);
       }
     });
   }
+
+  runGame("addition");
 });
 
 /**
  * The main game "loop", called when the script is first loaded and after the
  * user's answer has been processed.
  */
-function runGame() {
+function runGame(gameType) {
 
   function generateRandomNumber(min, max) {
     return (Math.floor(Math.random() * max) + min);
@@ -27,12 +29,23 @@ function runGame() {
 
   let num1 = generateRandomNumber(1, 25);
   let num2 = generateRandomNumber(1, 25);
+
+  if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+  } else {
+    alert(`Unknown Game Type: ${gameType}`);
+    throw `Unknown Game Type: ${gameType}. Aborting`;
+  }
 }
 
 function checkAnswer() { }
 function calculateCorrectAnswer() { }
 function incrementScore() { }
 function displayQuestion() { }
-function displayAdditionQuestion() { }
+function displayAdditionQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "+";
+}
 function displaySubtractQuestion() { }
 function displayMultiplyQuestion() { }
